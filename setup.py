a48f84e27bb7bd41179b2f0a5dd405f9220bd973
SirJoe <mail.wagenleitner@gmail.com>
2019-06-18 13:25:50 +0200

    new version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="igramscraper",
-    version="0.2.4",
+    version="0.3.0",
     description=('scrapes medias, likes, followers, tags and all metadata'),
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",

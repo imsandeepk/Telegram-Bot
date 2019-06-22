adce65c9cd4c96d2f2d722d1993ad9aa1bfeb10d
SirJoe <mail.wagenleitner@gmail.com>
2019-06-22 13:26:08 +0200

    removed python versions from setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries'
     ],

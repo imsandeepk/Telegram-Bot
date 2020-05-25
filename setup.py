579d80a77378800c1f827593d714f61fdcc45cae
Alejandro Perez Cosio <alejandroperezcosio@gmail.com>
2020-05-25 13:46:29 -0400

    Allow package to be installed in others python 3 versions (#104)
    thanks!

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries'
     ],
 )
